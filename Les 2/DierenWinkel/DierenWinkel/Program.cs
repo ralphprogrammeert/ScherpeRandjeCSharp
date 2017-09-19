4a284f24dc8ab1ce71c4a0a4613080f368d8d278
Ralph van der Neut <ralph.van.der.neut@zenvoices.com>
2017-09-19 20:09:44 +0200

    added les 2

---
+++ Les 2/DierenWinkel/DierenWinkel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DierenWinkel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Goedemorgen!");
+            // Hier willen we dat de klant alleen j(a) of n(ee) typt
+            Console.WriteLine("Kan ik u ergens mee van dienst zijn? (j/n)");
+            Console.ReadLine();
+            Console.WriteLine("Sorry, ik weet dat niet!");
+            Console.WriteLine("Moet ik de manager halen? (j/n)");
+            Console.ReadLine();
+            Console.WriteLine("Sorry, hij is er niet");
+            Console.Write("Tot");
+            Console.Write(" ");
+            Console.WriteLine("ziens");
+            Console.ReadLine();
+        }
+    }
+}
